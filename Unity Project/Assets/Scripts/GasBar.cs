b2cba062b705b68c1db2c7eb11c32ae59f6803cd
Dalon <s.laimer@gmx.de>
2019-04-28 15:54:02 +0200

    some balancing and bug fixes

--- Unity Project/Assets/Scripts/GasBar.cs
+++ Unity Project/Assets/Scripts/GasBar.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 
 public class GasBar : MonoBehaviour
 {
+    [SerializeField] RectTransform BarContainer;
     private Image _img;
-    private Player _player;
+    private Player _player;   
+    private float _baseLength;
 
     // Start is called before the first frame update
     void Start()
     {
+        _baseLength = BarContainer.rect.width;
         _img = GetComponent<Image>();
     }
 
@@ -19,6 +22,11 @@ public class GasBar : MonoBehaviour
     {
         if (!GameManager.Instance.Started) return;
 
-        _img.fillAmount = GameManager.Instance.Player.Gas;
+        _img.fillAmount = GameManager.Instance.Player.Gas / GameManager.Instance.Player.MaxGas;
+    }
+
+    public void UpdateBarLength()
+    {
+        BarContainer.sizeDelta = new Vector2(_baseLength * GameManager.Instance.Player.MaxGas, BarContainer.sizeDelta.y);
     }
 }
